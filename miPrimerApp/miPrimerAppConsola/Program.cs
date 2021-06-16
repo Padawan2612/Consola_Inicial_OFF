f17e757e263e50a062863be9f27af48f72aeda90
Padawan2612 <mjoeln2612@hotmail.com>
2021-06-15 21:36:40 -0500

    consola_inicial
    Aplicacion de consola c#

---
+++ miPrimerApp/miPrimerAppConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace miPrimerAppConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("za warudo");
+        }
+    }
+}
